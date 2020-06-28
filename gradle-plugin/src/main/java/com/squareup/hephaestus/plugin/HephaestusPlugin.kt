f72740c2bd3ad0392643aec5193e1f9c2ec7a193
Ralf Wondratschek <ralf@squareup.com>
2020-06-27 21:30:24 -0700

    Avoid eager configuration of Kotlin compile tasks. The `all(action)` method forces realizing all tasks for the given type.
    In the new way we register our custom tasks. When the Kotlin compile tasks are configured, then we
    hook up our custom task.

--- gradle-plugin/src/main/java/com/squareup/hephaestus/plugin/HephaestusPlugin.kt
+++ gradle-plugin/src/main/java/com/squareup/hephaestus/plugin/HephaestusPlugin.kt
@@ -4,12 +4,14 @@ import com.android.build.gradle.AppExtension
 import com.android.build.gradle.AppPlugin
 import com.android.build.gradle.LibraryExtension
 import com.android.build.gradle.LibraryPlugin
+import com.android.build.gradle.TestExtension
+import com.android.build.gradle.TestedExtension
 import com.android.build.gradle.api.BaseVariant
-import org.gradle.api.Action
 import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.AppliedPlugin
+import org.gradle.api.plugins.PluginManager
 import org.jetbrains.kotlin.gradle.internal.KaptGenerateStubsTask
 import org.jetbrains.kotlin.gradle.plugin.KaptExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
@@ -21,15 +23,28 @@ open class HephaestusPlugin : Plugin<Project> {
   @ExperimentalStdlibApi
   override fun apply(project: Project) {
     val once = AtomicBoolean()
-    val innerApply = Action<AppliedPlugin> {
-      if (once.compareAndSet(false, true)) {
-        realApply(project)
+
+    fun PluginManager.withPluginOnce(
+      id: String,
+      action: (AppliedPlugin) -> Unit
+    ) {
+      withPlugin(id) {
+        if (once.compareAndSet(false, true)) {
+          action(it)
+        }
       }
     }
-    with(project.pluginManager) {
-      withPlugin("org.jetbrains.kotlin.android", innerApply)
-      withPlugin("org.jetbrains.kotlin.jvm", innerApply)
+
+    // Apply the Hephaestus plugin after the Kotlin plugin was applied. There could be a timing
+    // issue. Also make sure to apply it only once. A module could accidentally apply the JVM and
+    // Android Kotlin plugin.
+    project.pluginManager.withPluginOnce("org.jetbrains.kotlin.android") {
+      realApply(project, true)
     }
+    project.pluginManager.withPluginOnce("org.jetbrains.kotlin.jvm") {
+      realApply(project, false)
+    }
+
     project.afterEvaluate {
       if (!once.get()) {
         throw GradleException(
@@ -41,8 +56,11 @@ open class HephaestusPlugin : Plugin<Project> {
   }
 
   @ExperimentalStdlibApi
-  private fun realApply(project: Project) {
-    disableIncrementalKotlinCompilation(project)
+  private fun realApply(
+    project: Project,
+    isAndroidProject: Boolean
+  ) {
+    disableIncrementalKotlinCompilation(project, isAndroidProject)
     disablePreciseJavaTracking(project)
 
     project.pluginManager.withPlugin("org.jetbrains.kotlin.kapt") {
@@ -75,47 +93,60 @@ open class HephaestusPlugin : Plugin<Project> {
 
   @ExperimentalStdlibApi
   private fun disableIncrementalKotlinCompilation(
-    project: Project
+    project: Project,
+    isAndroidProject: Boolean
   ) {
     project.tasks
-        .withType(KotlinCompile::class.java)
-        .all { compileTask ->
-          if (compileTask is KaptGenerateStubsTask) {
-            // Disable incremental compilation for the stub generating task. If any of the
-            // dependencies in the compile classpath have changed, then this we need to trigger the
-            // compiler plugin. This will make sure that we pick up any change from a dependency
-            // when merging all the classes. Without this workaround we could make changes in any
-            // library, but these changes wouldn't be contributed to the Dagger graph, because
-            // incremental compilation tricked us.
-            compileTask.doFirst {
-              compileTask.incremental = false
-              compileTask.logger.info(
-                  "Hephaestus: Incremental compilation enabled: ${compileTask.incremental} (stub)"
-              )
-            }
-            return@all
-          }
-
-          // Disable incremental compilation, if the compiler plugin dependency isn't up-to-date.
-          // This will trigger a full compilation of a module using Hephaestus even though its
-          // source files might not have changed. This workaround is necessary, otherwise
-          // incremental builds are broken. See https://youtrack.jetbrains.com/issue/KT-38570
-          val name = compileTask.name + "CheckIncrementalCompilationHephaestus"
-          compileTask.dependsOn(
-              project.tasks.register(name, DisableIncrementalCompilationTask::class.java)
-          )
-
-          compileTask.doFirst {
-            val task = project.tasks.getByName(name) as DisableIncrementalCompilationTask
-            if (task.didClassPathChange) {
-              compileTask.incremental = false
-            }
-
-            compileTask.logger.info(
-                "Hephaestus: Incremental compilation enabled: ${compileTask.incremental} (compile)"
+        .withType(KaptGenerateStubsTask::class.java)
+        .configureEach { stubsTask ->
+          // Disable incremental compilation for the stub generating task. Trigger the compiler
+          // plugin if any dependencies in the compile classpath have changed. This will make sure
+          // that we pick up any change from a dependency when merging all the classes. Without
+          // this workaround we could make changes in any library, but these changes wouldn't be
+          // contributed to the Dagger graph, because incremental compilation tricked us.
+          stubsTask.doFirst {
+            stubsTask.incremental = false
+            stubsTask.logger.info(
+                "Hephaestus: Incremental compilation enabled: ${stubsTask.incremental} (stub)"
             )
           }
         }
+
+    val disableIncrementalCompilationAction: (String) -> Unit = { compileTaskName ->
+      // Disable incremental compilation, if the compiler plugin dependency isn't up-to-date.
+      // This will trigger a full compilation of a module using Hephaestus even though its
+      // source files might not have changed. This workaround is necessary, otherwise
+      // incremental builds are broken. See https://youtrack.jetbrains.com/issue/KT-38570
+      val disableIncrementalCompilationTaskProvider = project.tasks.register(
+          compileTaskName + "CheckIncrementalCompilationHephaestus",
+          DisableIncrementalCompilationTask::class.java
+      )
+
+      project.tasks.named(compileTaskName, KotlinCompile::class.java) { compileTask ->
+        compileTask.dependsOn(disableIncrementalCompilationTaskProvider)
+
+        compileTask.doFirst {
+          if (disableIncrementalCompilationTaskProvider.get().didClassPathChange) {
+            compileTask.incremental = false
+          }
+
+          compileTask.logger.info(
+              "Hephaestus: Incremental compilation enabled: ${compileTask.incremental} (compile)"
+          )
+        }
+      }
+    }
+
+    if (isAndroidProject) {
+      project.androidVariantsConfigure { variant ->
+        val compileTaskName = "compile${variant.name.capitalize(US)}Kotlin"
+        disableIncrementalCompilationAction(compileTaskName)
+      }
+    } else {
+      // The Java plugin has two Kotlin tasks we care about: compileKotlin and compileTestKotlin.
+      disableIncrementalCompilationAction("compileKotlin")
+      disableIncrementalCompilationAction("compileTestKotlin")
+    }
   }
 }
 
@@ -129,6 +160,27 @@ fun Project.androidVariants(): Set<BaseVariant> {
     is LibraryExtension -> androidExtension.libraryVariants + androidExtension.testVariants +
         androidExtension.unitTestVariants
     else -> throw GradleException("Unknown Android module type for project ${project.path}")
+  }
+}
+
+/**
+ * Runs the given [action] for each Android variant including androidTest and unit test variants.
+ */
+fun Project.androidVariantsConfigure(action: (BaseVariant) -> Unit) {
+  val androidExtension = project.extensions.findByName("android")
+
+  if (androidExtension is AppExtension) {
+    androidExtension.applicationVariants.configureEach(action)
+  }
+  if (androidExtension is LibraryExtension) {
+    androidExtension.libraryVariants.configureEach(action)
+  }
+  if (androidExtension is TestExtension) {
+    androidExtension.applicationVariants.configureEach(action)
+  }
+  if (androidExtension is TestedExtension) {
+    androidExtension.unitTestVariants.configureEach(action)
+    androidExtension.testVariants.configureEach(action)
   }
 }
 
