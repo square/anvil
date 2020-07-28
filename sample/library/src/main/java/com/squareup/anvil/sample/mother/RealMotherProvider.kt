a0b0f4d7cdde2e50bca0615d2c3bb82b94e26818
Ralf Wondratschek <ralf@squareup.com>
2020-07-28 16:08:31 -0700

    Use the new @ContributesBinding annotation in the sample project.

--- sample/library/src/main/java/com/squareup/anvil/sample/mother/RealMotherProvider.kt
+++ sample/library/src/main/java/com/squareup/anvil/sample/mother/RealMotherProvider.kt
@@ -1,11 +1,14 @@
 package com.squareup.anvil.sample.mother
 
+import com.squareup.anvil.annotations.ContributesBinding
 import com.squareup.anvil.sample.God
 import com.squareup.anvil.sample.God.HEPHAESTUS
 import com.squareup.anvil.sample.God.HERA
 import com.squareup.anvil.sample.God.ZEUS
+import com.squareup.scopes.AppScope
 import javax.inject.Inject
 
+@ContributesBinding(AppScope::class)
 class RealMotherProvider @Inject constructor() : MotherProvider {
   override fun mother(god: God): String =
     when (god) {
