9dd1f03c98bc007ff054af720145d4b42ef1eb4e
Ralf Wondratschek <ralf@squareup.com>
2020-08-29 20:34:51 -0700

    Write a script that let's you easily compare the size of generated code between Dagger and Anvil. This helps working on #76.

---
+++ compare_size.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./gradlew :integration-tests:library:clean :integration-tests:library:assemble --no-build-cache -Psquare.generateDaggerFactoriesWithAnvil=false
+
+mv integration-tests/library/build/libs/library.jar integration-tests/library/dagger.jar
+
+./gradlew :integration-tests:library:clean :integration-tests:library:assemble --no-build-cache -Psquare.generateDaggerFactoriesWithAnvil=true
+
+mv integration-tests/library/build/libs/library.jar integration-tests/library/build/libs/anvil.jar
+mv integration-tests/library/dagger.jar integration-tests/library/build/libs/dagger.jar
+
+diffuse diff --jar integration-tests/library/build/libs/dagger.jar integration-tests/library/build/libs/anvil.jar
