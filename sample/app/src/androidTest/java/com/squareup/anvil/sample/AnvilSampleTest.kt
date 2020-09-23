776205eb749fcb0167788fa84d5053b3702344ca
Ralf Wondratschek <ralf@squareup.com>
2020-09-23 11:06:26 -0700

    Use the @ContributesBinding annotation in the sample. It's the recommended way for binding implementations to interfaces.

--- sample/app/src/androidTest/java/com/squareup/anvil/sample/AnvilSampleTest.kt
+++ sample/app/src/androidTest/java/com/squareup/anvil/sample/AnvilSampleTest.kt
@@ -1,10 +1,13 @@
 package com.squareup.anvil.sample
 
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import com.google.common.truth.Truth.assertThat
+import com.squareup.scopes.ComponentHolder
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,7 +19,11 @@ class AnvilSampleTest {
   val activityRule = ActivityTestRule(MainActivity::class.java)
 
   @Test fun text_is_displayed() {
-    Espresso.onView(ViewMatchers.withText("Hephaestus, son of (No Father) and Hera"))
-        .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    onView(withText("Hephaestus, son of (No Father) and Hera")).check(matches(isDisplayed()))
+  }
+
+  @Test fun fatherProvider_is_fake_instance() {
+    val fatherProvider = ComponentHolder.component<DescriptionComponent>().fatherProvider()
+    assertThat(fatherProvider).isSameInstanceAs(FakeFatherProvider)
   }
 }
