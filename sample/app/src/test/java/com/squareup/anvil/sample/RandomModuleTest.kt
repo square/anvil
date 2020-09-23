578b70f0de2acca17468c614d59e874c6c7af6c8
Angella Derington <aderington@dropbox.com>
2020-09-23 12:33:56 -0700

    Remove unnecessary component retrieval

--- sample/app/src/test/java/com/squareup/anvil/sample/RandomModuleTest.kt
+++ sample/app/src/test/java/com/squareup/anvil/sample/RandomModuleTest.kt
@@ -1,18 +1,17 @@
 package com.squareup.anvil.sample
 
-import com.google.common.truth.Truth.assertThat
 import com.google.common.truth.Truth.assertWithMessage
-import com.squareup.scopes.ComponentHolder
 import org.junit.Test
 
 class RandomModuleTest {
-  @Test fun `module in this app module is contributed`() {
-    ComponentHolder.components.add(DaggerAppComponent.create())
-
-    val component = ComponentHolder.component<RandomComponent>()
+  @Test
+  fun `module in this app module is contributed`() {
+    val component = DaggerAppComponent.create() as RandomComponent
 
     // Verify
-    assertWithMessage("Module should provide a non-empty string").that(component.string()).isNotEmpty()
-    assertWithMessage("Module should provide a deterministic string").that(component.string()).isEqualTo(component.string())
+    assertWithMessage("Module should provide a non-empty string").that(component.string())
+      .isNotEmpty()
+    assertWithMessage("Module should provide a deterministic string").that(component.string())
+      .isEqualTo(component.string())
   }
 }
