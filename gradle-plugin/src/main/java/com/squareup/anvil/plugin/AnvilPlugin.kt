21baf539fd5562befc5baf4e2339c16a1004075f
Rick Busarow <rickbusarow@gmail.com>
2021-06-08 09:55:25 -0500

    remove unused import

--- gradle-plugin/src/main/java/com/squareup/anvil/plugin/AnvilPlugin.kt
+++ gradle-plugin/src/main/java/com/squareup/anvil/plugin/AnvilPlugin.kt
@@ -14,7 +14,6 @@ import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.UnknownTaskException
 import org.gradle.api.artifacts.Configuration
-import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.TaskContainer
 import org.gradle.api.tasks.TaskProvider
