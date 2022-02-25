9e7ad7bb72e765d326b17c82aeaea5114352f6e4
Joel Wilcox <jwilcox@squareup.com>
2022-02-25 12:22:36 -0800

    Migrate the remainder of `ModuleMergerIr` to use Reference classes

--- compiler/src/main/java/com/squareup/anvil/compiler/ClassScannerIr.kt
+++ compiler/src/main/java/com/squareup/anvil/compiler/ClassScannerIr.kt
@@ -2,16 +2,15 @@ package com.squareup.anvil.compiler
 
 import com.squareup.anvil.compiler.codegen.reference.ClassReferenceIr
 import com.squareup.anvil.compiler.codegen.reference.RealAnvilModuleDescriptor
+import com.squareup.anvil.compiler.codegen.reference.toClassReference
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
-import org.jetbrains.kotlin.ir.symbols.IrClassSymbol
 import org.jetbrains.kotlin.name.FqName
 
 /**
  * Returns a sequence of contributed classes from the dependency graph. Note that the result
  * includes inner classes already.
  */
-// TODO: Change to return [ClassReferenceIr]
 internal fun ClassScanner.findContributedClasses(
   pluginContext: IrPluginContext,
   moduleFragment: IrModuleFragment,
@@ -19,11 +18,11 @@ internal fun ClassScanner.findContributedClasses(
   annotation: FqName,
   scope: ClassReferenceIr?,
   moduleDescriptorFactory: RealAnvilModuleDescriptor.Factory
-): Sequence<IrClassSymbol> {
+): Sequence<ClassReferenceIr> {
   val module = moduleDescriptorFactory.create(moduleFragment.descriptor)
 
   return findContributedClasses(module, packageName, annotation, scope?.fqName)
     .map {
-      pluginContext.requireReferenceClass(it.fqName)
+      pluginContext.requireReferenceClass(it.fqName).toClassReference(pluginContext)
     }
 }
