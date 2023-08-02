e0735f31c141d50781d73bf56f1f0f922c3f096e
Zac Sweers <pandanomic@gmail.com>
2023-08-01 21:50:55 -0800

    Finish test support + add simple symbol processor support

--- compiler/src/test/java/com/squareup/anvil/compiler/codegen/ContributesMultibindingGeneratorTest.kt
+++ compiler/src/test/java/com/squareup/anvil/compiler/codegen/ContributesMultibindingGeneratorTest.kt
@@ -1,6 +1,8 @@
 package com.squareup.anvil.compiler.codegen
 
 import com.google.common.truth.Truth.assertThat
+import com.squareup.anvil.annotations.MergeComponent
+import com.squareup.anvil.compiler.codegen.ksp.simpleSymbolProcessor
 import com.squareup.anvil.compiler.compile
 import com.squareup.anvil.compiler.contributingInterface
 import com.squareup.anvil.compiler.hintMultibinding
@@ -13,7 +15,6 @@ import com.squareup.anvil.compiler.mergeComponentFqName
 import com.squareup.anvil.compiler.secondContributingInterface
 import com.squareup.anvil.compiler.walkGeneratedFiles
 import com.tschuchort.compiletesting.KotlinCompilation.ExitCode.OK
-import org.junit.Assume.assumeTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
@@ -28,7 +29,7 @@ class ContributesMultibindingGeneratorTest(
     @Parameterized.Parameters(name = "{0}")
     @JvmStatic fun modes(): Collection<Any> {
       return buildList {
-//        add(AnvilCompilationMode.Embedded())
+        add(AnvilCompilationMode.Embedded())
         add(AnvilCompilationMode.Ksp())
       }
     }
@@ -361,25 +362,38 @@ class ContributesMultibindingGeneratorTest(
   }
 
   @Test fun `a contributed multibinding can be generated`() {
-    assumeTrue(mode is AnvilCompilationMode.Embedded)
-    val codeGenerator = simpleCodeGenerator { clazz ->
-      clazz
-        .takeIf { it.isAnnotatedWith(mergeComponentFqName) }
-        ?.let {
-          //language=kotlin
-          """
-            package com.squareup.test
-                
-            import com.squareup.anvil.annotations.ContributesMultibinding
-            import dagger.MapKey
-            import javax.inject.Singleton
+    val stubContentToGenerate =
+      //language=kotlin
+      """
+          package com.squareup.test
+              
+          import com.squareup.anvil.annotations.ContributesMultibinding
+          import dagger.MapKey
+          import javax.inject.Singleton
+       
+          @ContributesMultibinding(Any::class)
+          @BindingKey("abc")
+          @Singleton
+          interface ContributingInterface : ParentInterface
+      """.trimIndent()
 
-            @ContributesMultibinding(Any::class)
-            @BindingKey("abc")
-            @Singleton
-            interface ContributingInterface : ParentInterface
-          """.trimIndent()
+    val localMode = when (mode) {
+      is AnvilCompilationMode.Embedded -> {
+        val codeGenerator = simpleCodeGenerator { clazz ->
+          clazz
+            .takeIf { it.isAnnotatedWith(mergeComponentFqName) }
+            ?.let { stubContentToGenerate }
         }
+        AnvilCompilationMode.Embedded(listOf(codeGenerator))
+      }
+      is AnvilCompilationMode.Ksp -> {
+        val processor = simpleSymbolProcessor { resolver ->
+          resolver.getSymbolsWithAnnotation(MergeComponent::class.qualifiedName!!)
+            .map { stubContentToGenerate }
+            .toList()
+        }
+        AnvilCompilationMode.Ksp(listOf(processor))
+      }
     }
 
     compile(
@@ -397,7 +411,7 @@ class ContributesMultibindingGeneratorTest(
         @MergeComponent(Any::class)
         interface ComponentInterface
       """,
-      mode = AnvilCompilationMode.Embedded(listOf(codeGenerator))
+      mode = localMode
     ) {
       assertThat(exitCode).isEqualTo(OK)
       assertThat(contributingInterface.hintMultibindingScope).isEqualTo(Any::class)
@@ -405,30 +419,45 @@ class ContributesMultibindingGeneratorTest(
   }
 
   @Test fun `a contributed multibinding can be generated with map keys being generated`() {
-    assumeTrue(mode is AnvilCompilationMode.Embedded)
-    val codeGenerator = simpleCodeGenerator { clazz ->
-      clazz
-        .takeIf { it.isAnnotatedWith(mergeComponentFqName) }
-        ?.let {
-          //language=kotlin
-          """
-            package com.squareup.test
-                
-            import com.squareup.anvil.annotations.ContributesMultibinding
-            import dagger.MapKey
-            import javax.inject.Singleton
+    val stubContentToGenerate =
+      //language=kotlin
+      """
+          package com.squareup.test
+              
+          import com.squareup.anvil.annotations.ContributesMultibinding
+          import dagger.MapKey
+          import javax.inject.Singleton
 
-            interface ParentInterface
+          interface ParentInterface
 
-            @MapKey
-            annotation class BindingKey1(val value: String)
+          @MapKey
+          annotation class BindingKey1(val value: String)
       
-            @ContributesMultibinding(Any::class)
-            @BindingKey1("abc")
-            @Singleton
-            interface ContributingInterface : ParentInterface
-          """.trimIndent()
+          @ContributesMultibinding(Any::class)
+          @BindingKey1("abc")
+          @Singleton
+          interface ContributingInterface : ParentInterface
+      """.trimIndent()
+
+    val localMode = when (mode) {
+      is AnvilCompilationMode.Embedded -> {
+        val codeGenerator = simpleCodeGenerator { clazz ->
+          clazz
+            .takeIf { it.isAnnotatedWith(mergeComponentFqName) }
+            ?.let {
+              stubContentToGenerate
+            }
         }
+        AnvilCompilationMode.Embedded(listOf(codeGenerator))
+      }
+      is AnvilCompilationMode.Ksp -> {
+        val processor = simpleSymbolProcessor { resolver ->
+          resolver.getSymbolsWithAnnotation(MergeComponent::class.qualifiedName!!)
+            .map { stubContentToGenerate }
+            .toList()
+        }
+        AnvilCompilationMode.Ksp(listOf(processor))
+      }
     }
 
     compile(
@@ -440,7 +469,7 @@ class ContributesMultibindingGeneratorTest(
         @MergeComponent(Any::class)
         interface ComponentInterface
       """,
-      mode = AnvilCompilationMode.Embedded(listOf(codeGenerator))
+      mode = localMode
     ) {
       assertThat(exitCode).isEqualTo(OK)
       assertThat(contributingInterface.hintMultibindingScope).isEqualTo(Any::class)
