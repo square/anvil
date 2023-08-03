f555ec1c7f06a35bb3434de77571d6b8f3093b56
Gabriel Ittner <gabriel.ittner@gmail.com>
2023-08-03 07:49:03 +0200

    run ktlint on the Gradle plugin

--- gradle-plugin/src/main/java/com/squareup/anvil/plugin/AnvilExtension.kt
+++ gradle-plugin/src/main/java/com/squareup/anvil/plugin/AnvilExtension.kt
@@ -51,7 +51,7 @@ public abstract class AnvilExtension @Inject constructor(objects: ObjectFactory)
     .convention(false)
 
   /**
-   * Add the `annotations-optional` artifact as a dependency to make annotations like `@SingleIn` and `@ForScope` 
+   * Add the `annotations-optional` artifact as a dependency to make annotations like `@SingleIn` and `@ForScope`
    * available to use. These are annotations that are not strictly required but which we've found to be helpful with managing larger dependency graphs.
    */
   public val addOptionalAnnotations: Property<Boolean> = objects.property(Boolean::class.java)
