bc7bc4f4cdd8bcd3fe4c2192e4f3956358a41e88
Julio Perez <jperez@slack-corp.com>
2023-10-24 12:01:13 -0700

    Update ComponentDetectorCheck.kt
    Fixed lint formatting issues.

--- compiler/src/main/java/com/squareup/anvil/compiler/codegen/dagger/ComponentDetectorCheck.kt
+++ compiler/src/main/java/com/squareup/anvil/compiler/codegen/dagger/ComponentDetectorCheck.kt
@@ -15,30 +15,30 @@ import com.squareup.anvil.compiler.codegen.ksp.KspAnvilException
 import com.squareup.anvil.compiler.daggerComponentFqName
 import com.squareup.anvil.compiler.internal.reference.AnvilCompilationExceptionClassReference
 import com.squareup.anvil.compiler.internal.reference.classAndInnerClassReferences
-import java.io.File
 import org.jetbrains.kotlin.descriptors.ModuleDescriptor
 import org.jetbrains.kotlin.psi.KtFile
+import java.io.File
 
 internal object ComponentDetectorCheck : AnvilApplicabilityChecker {
   private const val MESSAGE =
-      "Anvil cannot generate the code for Dagger components or subcomponents. In " +
-          "these cases the Dagger annotation processor is required. Enabling the Dagger " +
-          "annotation processor and turning on Anvil to generate Dagger factories is " +
-          "redundant. Set 'generateDaggerFactories' to false."
+    "Anvil cannot generate the code for Dagger components or subcomponents. In " +
+      "these cases the Dagger annotation processor is required. Enabling the Dagger " +
+      "annotation processor and turning on Anvil to generate Dagger factories is " +
+      "redundant. Set 'generateDaggerFactories' to false."
   private val ANNOTATIONS_TO_CHECK = setOf(daggerComponentFqName)
   override fun isApplicable(context: AnvilContext) = context.generateFactories
 
   internal class KspGenerator(
-      override val env: SymbolProcessorEnvironment,
+    override val env: SymbolProcessorEnvironment,
   ) : AnvilSymbolProcessor() {
     @AutoService(SymbolProcessorProvider::class)
     class Provider : AnvilSymbolProcessorProvider(ComponentDetectorCheck, ::KspGenerator)
 
     override fun processChecked(resolver: Resolver): List<KSAnnotated> {
       val clazz =
-          ANNOTATIONS_TO_CHECK.flatMap { resolver.getSymbolsWithAnnotation(it.asString()) }
-              .firstOrNull()
-              ?: return emptyList()
+        ANNOTATIONS_TO_CHECK.flatMap { resolver.getSymbolsWithAnnotation(it.asString()) }
+          .firstOrNull()
+          ?: return emptyList()
 
       throw KspAnvilException(message = MESSAGE, node = clazz)
     }
@@ -51,19 +51,20 @@ internal object ComponentDetectorCheck : AnvilApplicabilityChecker {
       ComponentDetectorCheck.isApplicable(context)
 
     override fun generateCodePrivate(
-        codeGenDir: File,
-        module: ModuleDescriptor,
-        projectFiles: Collection<KtFile>
+      codeGenDir: File,
+      module: ModuleDescriptor,
+      projectFiles: Collection<KtFile>
     ) {
       val clazz =
-          projectFiles.classAndInnerClassReferences(module).firstOrNull {
-            clazz -> ANNOTATIONS_TO_CHECK.any { clazz.isAnnotatedWith(it) }
-          }
+        projectFiles.classAndInnerClassReferences(module).firstOrNull {
+          clazz ->
+          ANNOTATIONS_TO_CHECK.any { clazz.isAnnotatedWith(it) }
+        }
 
       if (clazz != null) {
         throw AnvilCompilationExceptionClassReference(
-            message = MESSAGE,
-            classReference = clazz
+          message = MESSAGE,
+          classReference = clazz
         )
       }
     }
