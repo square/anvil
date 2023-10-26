68c66a0cbf3b7f66568450269caea58533310435
Rick Busarow <rickbusarow@gmail.com>
2023-10-26 17:07:54 -0500

    modernize Anvil's publishing

---
+++ build-logic/src/main/kotlin/com/squareup/anvil/KtlintConventionPlugin.kt
@@ -0,0 +1,22 @@
+package com.squareup.anvil
+
+import com.rickbusarow.kgx.libsCatalog
+import com.rickbusarow.kgx.version
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+import org.jlleitschuh.gradle.ktlint.KtlintPlugin
+
+open class KtlintConventionPlugin : Plugin<Project> {
+
+  override fun apply(target: Project) {
+    target.plugins.apply(KtlintPlugin::class.java)
+
+    target.plugins.apply(org.jlleitschuh.gradle.ktlint.KtlintPlugin::class.java)
+
+    target.extensions.configure(KtlintExtension::class.java) { ktlint ->
+      ktlint.version.set(target.libsCatalog.version("ktlint"))
+      ktlint.verbose.set(true)
+    }
+  }
+}
--- build-logic/src/main/kotlin/com/squareup/anvil/RootPlugin.kt
+++ build-logic/src/main/kotlin/com/squareup/anvil/RootPlugin.kt
@@ -3,16 +3,18 @@ package com.squareup.anvil
 import com.squareup.anvil.benchmark.BenchmarkPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformJvmPlugin
 
 open class RootPlugin : Plugin<Project> {
   override fun apply(target: Project) {
-
     target.plugins.apply(BenchmarkPlugin::class.java)
+    target.plugins.apply(KtlintConventionPlugin::class.java)
   }
 }
 
 open class LibraryPlugin : Plugin<Project> {
   override fun apply(target: Project) {
-    // TODO
+    target.plugins.apply(KotlinPlatformJvmPlugin::class.java)
+    target.plugins.apply(KtlintConventionPlugin::class.java)
   }
 }
