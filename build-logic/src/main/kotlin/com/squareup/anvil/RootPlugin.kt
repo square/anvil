7bcd9e86c5e1f07dabbb8854d15a11042af9b80f
Rick Busarow <rickbusarow@gmail.com>
2023-10-27 22:29:01 -0500

    Move the `:gradle-plugin` project into the main build
    Inspired by Molecule [#131](https://github.com/cashapp/molecule/pull/131)
    
    The `:gradle-plugin` project is now part of two builds.
    
    The "build-logic" build creates a version that can be applied to projects in the main build (samples).  This sits in between the normal `build-logic` build and the main build, so that the `:gradle-plugin` project can still consume the convention plugins.
    
    The "main" build creates the version that's actually published to Maven Central and the plugin portal.  The `:gradle-plugin` project no longer needs special treatment when publishing or executing CI.

--- build-logic/src/main/kotlin/com/squareup/anvil/RootPlugin.kt
+++ build-logic/src/main/kotlin/com/squareup/anvil/RootPlugin.kt
@@ -1,9 +1,10 @@
 package com.squareup.anvil
 
+import com.rickbusarow.kgx.libsCatalog
+import com.rickbusarow.kgx.pluginId
 import com.squareup.anvil.benchmark.BenchmarkPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformJvmPlugin
 
 open class RootPlugin : Plugin<Project> {
   override fun apply(target: Project) {
@@ -14,7 +15,7 @@ open class RootPlugin : Plugin<Project> {
 
 open class LibraryPlugin : Plugin<Project> {
   override fun apply(target: Project) {
-    target.plugins.apply(KotlinPlatformJvmPlugin::class.java)
+    target.plugins.apply(target.libsCatalog.pluginId("kotlin-jvm"))
     target.plugins.apply(KtlintConventionPlugin::class.java)
   }
 }
