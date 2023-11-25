b5cf6bfab231fde9d62117a63cef482585601a41
Zac Sweers <pandanomic@gmail.com>
2023-11-25 01:49:40 -0500

    Test fixes in AssistedFactoryGeneratorTest

--- compiler/src/test/java/com/squareup/anvil/compiler/dagger/AssistedFactoryGeneratorTest.kt
+++ compiler/src/test/java/com/squareup/anvil/compiler/dagger/AssistedFactoryGeneratorTest.kt
@@ -4,9 +4,11 @@ import com.google.common.truth.Truth.assertThat
 import com.squareup.anvil.compiler.WARNINGS_AS_ERRORS
 import com.squareup.anvil.compiler.assistedService
 import com.squareup.anvil.compiler.assistedServiceFactory
+import com.squareup.anvil.compiler.compilationErrorLine
 import com.squareup.anvil.compiler.daggerModule1
 import com.squareup.anvil.compiler.internal.testing.AnvilCompilation
 import com.squareup.anvil.compiler.internal.testing.AnvilCompilationMode
+import com.squareup.anvil.compiler.internal.testing.AnvilCompilationMode.Embedded
 import com.squareup.anvil.compiler.internal.testing.createInstance
 import com.squareup.anvil.compiler.internal.testing.factoryClass
 import com.squareup.anvil.compiler.internal.testing.getPropertyValue
@@ -19,6 +21,7 @@ import com.squareup.anvil.compiler.useDaggerAndKspParams
 import com.tschuchort.compiletesting.JvmCompilationResult
 import com.tschuchort.compiletesting.KotlinCompilation.ExitCode.OK
 import org.intellij.lang.annotations.Language
+import org.junit.Assume.assumeTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
@@ -1639,8 +1642,7 @@ public final class AssistedServiceFactory_Impl implements AssistedServiceFactory
     ) {
       assertThat(exitCode).isError()
       assertThat(
-        messages.lines()
-          .first { it.startsWith("e:") }
+        compilationErrorLine()
           .removeParametersAndSort()
           .removeNullabilityAnnotations(),
       ).contains(
@@ -1677,8 +1679,7 @@ public final class AssistedServiceFactory_Impl implements AssistedServiceFactory
     ) {
       assertThat(exitCode).isError()
       assertThat(
-        messages.lines()
-          .first { it.startsWith("e:") }
+        compilationErrorLine()
           .removeParametersAndSort()
           .removeNullabilityAnnotations(),
       ).contains(
@@ -1712,8 +1713,7 @@ public final class AssistedServiceFactory_Impl implements AssistedServiceFactory
     ) {
       assertThat(exitCode).isError()
       assertThat(
-        messages.lines()
-          .first { it.startsWith("e:") }
+        compilationErrorLine()
           .removeParametersAndSort()
           .removeNullabilityAnnotations(),
       ).contains(
@@ -1813,6 +1813,8 @@ public final class AssistedServiceFactory_Impl implements AssistedServiceFactory
   }
 
   @Test fun `assisted injections can be provided with a qualifier`() {
+    // TODO enable on KSP after BindingModuleGenerator supports KSP
+    assumeTrue(mode is Embedded)
     compile(
       """
       package com.squareup.test
