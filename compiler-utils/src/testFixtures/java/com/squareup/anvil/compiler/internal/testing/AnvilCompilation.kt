64411d3468bac6b362ca95e70529d55203d5c414
Zac Sweers <pandanomic@gmail.com>
2023-12-25 00:37:10 -0600

    Add dagger processor to compilation when processing with dagger

--- compiler-utils/src/testFixtures/java/com/squareup/anvil/compiler/internal/testing/AnvilCompilation.kt
+++ compiler-utils/src/testFixtures/java/com/squareup/anvil/compiler/internal/testing/AnvilCompilation.kt
@@ -68,7 +68,10 @@ public class AnvilCompilation internal constructor(
         DaggerAnnotationProcessingMode.KSP -> {
           @Suppress("invisible_reference", "invisible_member")
           symbolProcessorProviders +=
-            listOf(com.squareup.anvil.compiler.ksp.MergeComponentSymbolProcessor.Provider())
+            listOf(
+              com.squareup.anvil.compiler.ksp.MergeComponentSymbolProcessor.Provider(),
+              KspComponentProcessor.Provider()
+            )
           // Run KSP in a single-pass
           // https://kotlinlang.slack.com/archives/C013BA8EQSE/p1639462548225400?thread_ts=1639433474.224900&cid=C013BA8EQSE
           kspWithCompilation = true
