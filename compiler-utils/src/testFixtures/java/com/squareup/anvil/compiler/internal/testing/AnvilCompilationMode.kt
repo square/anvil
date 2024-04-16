9f16cc17b6d0277cd23f45e11f4adb26af43e5e2
Zac Sweers <pandanomic@gmail.com>
2024-04-16 16:20:05 -0400

    Opportunistic: make compilation mode to strings easier to read

--- compiler-utils/src/testFixtures/java/com/squareup/anvil/compiler/internal/testing/AnvilCompilationMode.kt
+++ compiler-utils/src/testFixtures/java/com/squareup/anvil/compiler/internal/testing/AnvilCompilationMode.kt
@@ -9,9 +9,27 @@ import com.squareup.anvil.compiler.api.CodeGenerator
 public sealed class AnvilCompilationMode(public val analysisBackend: AnalysisBackend) {
   public data class Embedded(
     val codeGenerators: List<CodeGenerator> = emptyList(),
-  ) : AnvilCompilationMode(EMBEDDED)
+  ) : AnvilCompilationMode(EMBEDDED) {
+    override fun toString(): String {
+      return if (codeGenerators.isEmpty()) {
+        "Embedded"
+      } else {
+        "Embedded(codeGenerators=$codeGenerators)"
+      }
+    }
+  }
+
   public data class Ksp(
     val symbolProcessorProviders: List<SymbolProcessorProvider> = emptyList(),
     val useKSP2: Boolean = false,
-  ) : AnvilCompilationMode(KSP)
+  ) : AnvilCompilationMode(KSP) {
+    override fun toString(): String {
+      val processors = if (symbolProcessorProviders.isEmpty()) {
+        ""
+      } else {
+        "symbolProcessorProviders=$symbolProcessorProviders,"
+      }
+      return "Ksp(${processors}useKSP2=$useKSP2)"
+    }
+  }
 }
