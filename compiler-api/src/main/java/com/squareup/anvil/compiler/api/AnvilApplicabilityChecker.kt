44090ac2712ba6fa5f2aadab403b250610afe0b1
Zac Sweers <pandanomic@gmail.com>
2024-05-13 17:41:26 -0400

    Make AnvilApplicabilityChecker a fun interface

--- compiler-api/src/main/java/com/squareup/anvil/compiler/api/AnvilApplicabilityChecker.kt
+++ compiler-api/src/main/java/com/squareup/anvil/compiler/api/AnvilApplicabilityChecker.kt
@@ -1,6 +1,6 @@
 package com.squareup.anvil.compiler.api
 
-public interface AnvilApplicabilityChecker {
+public fun interface AnvilApplicabilityChecker {
   /**
    * Returns true if this code generator is applicable for the given [context] or false if not. This
    * will only be called _once_.
