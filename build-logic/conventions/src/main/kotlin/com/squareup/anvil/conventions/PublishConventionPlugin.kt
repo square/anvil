e728f056a9b4c9bfba094eb134a905925bb634ae
Zac Sweers <pandanomic@gmail.com>
2024-08-02 18:35:40 -0400

    Update maven-publish to 0.29.0 (#38)
    * Update maven-publish to 0.29.0
    
    * Don't publish to s01
    
    * Update snapshot publishing
    
    * format

--- build-logic/conventions/src/main/kotlin/com/squareup/anvil/conventions/PublishConventionPlugin.kt
+++ build-logic/conventions/src/main/kotlin/com/squareup/anvil/conventions/PublishConventionPlugin.kt
@@ -10,7 +10,6 @@ import com.squareup.anvil.conventions.utils.libs
 import com.vanniktech.maven.publish.JavadocJar.Dokka
 import com.vanniktech.maven.publish.KotlinJvm
 import com.vanniktech.maven.publish.MavenPublishBaseExtension
-import com.vanniktech.maven.publish.SonatypeHost
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.publish.maven.MavenPublication
@@ -38,7 +37,7 @@ open class PublishConventionPlugin : Plugin<Project> {
     @Suppress("UnstableApiUsage")
     mavenPublishing.pomFromGradleProperties()
     mavenPublishing.signAllPublications()
-    mavenPublishing.publishToMavenCentral(SonatypeHost.S01, automaticRelease = true)
+    mavenPublishing.publishToMavenCentral(automaticRelease = true)
 
     target.plugins.withId("org.jetbrains.kotlin.jvm") {
       when {
