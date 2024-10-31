ad9b60703553aab04ecc267200ccb9e08bf56c0d
Rick Busarow <rickbusarow@gmail.com>
2024-10-31 11:34:52 -0500

    remove the check for Gradle < 8.5 in LifecycleTest
    There's no need to test back that far anymore, and we don't.
    The simple version check we had doesn't work since "8.10" is less than "8.5",
    and there's no need to fix it instead of just removing it.

--- gradle-plugin/src/gradleTest/java/com/squareup/anvil/plugin/LifecycleTest.kt
+++ gradle-plugin/src/gradleTest/java/com/squareup/anvil/plugin/LifecycleTest.kt
@@ -26,7 +26,7 @@ class LifecycleTest : BaseGradleTest() {
   fun `tasks are compatible with configuration caching when targeting kotlin-jvm`() =
     params
       .distinctBy { it.gradleVersion }
-      .withKspToggle { versions, useKsp ->
+      .withKspToggle { _, useKsp ->
 
         rootProject {
           buildFile {
@@ -75,10 +75,8 @@ class LifecycleTest : BaseGradleTest() {
           }
         }
 
-        val calculatingMessage = when {
-          versions.gradleVersion >= "8.5" -> "Calculating task graph as no cached configuration is available for tasks: compileKotlin"
-          else -> "Calculating task graph as no configuration cache is available for tasks: compileKotlin"
-        }
+        val calculatingMessage =
+          "Calculating task graph as no cached configuration is available for tasks: compileKotlin"
 
         val storingMessage = "Configuration cache entry stored."
 
