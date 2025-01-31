cc67470e837b0a1715b8020d50ca392530c16fe7
Rick Busarow <rickbusarow@gmail.com>
2025-01-31 13:00:59 -0600

    introduce base classes for `AnvilFirExtension` in `:compiler-k2-api`

---
+++ compiler-k2-api/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+  id("conventions.library")
+  id("conventions.publish")
+}
+
+conventions {
+  explicitApi = true
+}
+
+publish {
+  configurePom(
+    artifactId = "compiler-k2-api",
+    pomName = "Anvil Compiler API for Kotlin 2+",
+    pomDescription = "API definitions for creating custom code generators that integrate with Anvil",
+  )
+}
+
+dependencies {
+  compileOnly(libs.auto.service.annotations)
+  api(libs.kotlin.compiler)
+
+  testImplementation(libs.junit.jupiter.api)
+  testImplementation(libs.kase)
+  testImplementation(libs.kotest.assertions.api)
+  testImplementation(libs.kotest.assertions.core.jvm)
+}
